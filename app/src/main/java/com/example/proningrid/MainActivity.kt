e10289cb20299a36b1bb4c6b5134c0199df58a80
komat0 <111368527+komat0@users.noreply.github.com>
2024-07-30 18:48:39 +0700

    Done!

--- app/src/main/java/com/example/proningrid/MainActivity.kt
+++ app/src/main/java/com/example/proningrid/MainActivity.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
